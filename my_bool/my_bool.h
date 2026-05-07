ff044798be9a911f3260b99112c928eee847127b
Augustin Mattei <augustin.mattei@epitech.eu>
2026-05-08 00:57:58 +0200

    feat: personal bool header

---
+++ my_bool/my_bool.h
@@ -0,0 +1,14 @@
+/*
+** my_libc project
+** my_bool.h based on the eponym libc header
+*/
+
+#ifndef MY_BOOL_H_
+    #define MY_BOOL_H_
+
+    #define true 1
+    #define false 0
+
+typedef unsigned char bool;
+
+#endif /* !MY_BOOL_H_ */
