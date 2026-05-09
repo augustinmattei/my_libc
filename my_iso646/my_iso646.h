41b9df46e09ef7bd2869ed60bee1bbcf17fe2ebb
Augustin Mattei <augustin.mattei@epitech.eu>
2026-05-09 21:26:22 +0200

    feat: iso646.h with operators defined because we include everyone in programming

---
+++ my_iso646/my_iso646.h
@@ -0,0 +1,21 @@
+/*
+** my_libc project
+** my_iso646.h based on the eponym libc header
+*/
+
+#ifndef MY_ISO646_H_
+    #define MY_ISO646_H_
+
+    #define and &&
+    #define and_eq &=
+    #define bitand &
+    #define bitor |
+    #define compl ~
+    #define not !
+    #define not_eq !=
+    #define or ||
+    #define or_eq |=
+    #define xor ^
+    #define xor_eq ^=
+
+#endif /* !MY_ISO646_H_ */
